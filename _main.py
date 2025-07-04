ad8341cbcb0211895db8626463a47a7c9ef8e972
Paulina Amelia Vega <ameliavega432@gmail.com>
2025-07-04 22:38:53 +0700

    upload model with lfs

--- _main.py
+++
@@ -1,5 +0,0 @@
-from run_model import run_svd_recommender
-
-
-if __name__ == "__main__":
-    svd_model = run_svd_recommender()
--- src/extract/extract_data.py
+++
@@ -1,8 +0,0 @@
-from google.colab import drive
-
-def mount_drive():
-    """
-    Mount Google Drive to access files stored there.
-    """
-    drive.mount('/content/drive')
-    
