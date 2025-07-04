be25e5ad2dc5b207eb0625b2c786f17202282ec0
Paulina Amelia Vega <ameliavega432@gmail.com>
2025-07-04 20:43:11 +0700

    remove if

--- UI.py
+++ UI.py
@@ -69,11 +69,11 @@ def set_background_image(image_file):
 def load_all_data():
     with st.spinner("Memuat model dan data awal... Ini mungkin butuh beberapa saat."):
         svd_model = run_svd_recommender()
-        st.write("SVD model loaded:", svd_model is not None)
+        st.write("SVD model loaded:", svd_model)
         st.write("Files in saved_models:", os.listdir("saved_models"))
-        if svd_model is None:
-            st.error("Gagal memuat model rekomendasi. Aplikasi tidak dapat berjalan.")
-            st.stop()
+        # if svd_model is None:
+        #     st.error("Gagal memuat model rekomendasi. Aplikasi tidak dapat berjalan.")
+        #     st.stop()
         
         try:
             movies_df = pd.read_csv(MOVIES_DATA_PATH)
